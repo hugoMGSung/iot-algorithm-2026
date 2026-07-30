f768ef44c441eb7872b3852fac42c6dbbb614f29
Hugo MG Sung <personar95@naver.com>
2026-07-30 14:59:27 +0900

    알고리즘 고급

--- advanced/algorithm01/app01/app01.cpp
+++ advanced/algorithm01/app01/app01.cpp
@@ -1,20 +1,105 @@
-﻿// app01.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// app01.cpp : 빌트인 데이터타입
 //
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a = 13;
+    int b = 4;
+    
+    cout << "정수 연산" << endl;
+    // 일반 연산
+    cout << a + b << endl; // 17
+    cout << a - b << endl; // 9
+    cout << a * b << endl; // 52
+    cout << a / b << endl; // 3
+    cout << a % b << endl; // 1
+    cout << -a << endl; // -13
+
+    // 비교 연산
+    cout << (a == b) << endl; // 0 false
+    cout << (a != b) << endl; // 1 true
+    cout << (a > b) << endl;  // 1
+    cout << (a < b) << endl; // 0
+    cout << (a >= b) << endl;  // 1
+    cout << (a <= b) << endl;  // 0
+
+    // 비트 연산
+    cout << (a & b) << endl; // 13 -> 1101     4 ->  0100  ==>   0100(4)
+    cout << (a | b) << endl;  // 1101 | 0100 -> 1101(13)
+
+    cout << "부동소수 연산" << endl;
+    double d = 2.5;
+    float f = 1.5f;
+
+    cout << sizeof(d) << " byte" << endl; // 8 byte
+    cout << sizeof(f) << " byte" << endl; // 4 byte
+
+    cout << d << " " << f << endl;  // 2.5 1.5
+    cout << d + f << endl;  // 4
+    cout << d - f << endl;  // 1
+    cout << d * f << endl;  // 3.75
+    cout << d / f << endl;  // 1.66667
+
+    // 형변환
+    cout << "형 변환" << endl;
+
+    int i = 65;
+    f = 5.2f;
+
+    // 암시적 형변환(메모리가 큰 변수로 변경)
+    d = i + f;   // i + f (일단 float 변경) -> double 할당(double 변경)
+    cout << d << endl;  // 70.2
+    // 명시적 형변환 double -> int
+    cout << static_cast<int>(d - 5) << endl; // 65
+    // 명시적 형변환 int -> char
+    cout << static_cast<char>(i) << endl; // 'A'
+
+    // 문자열
+    cout << "문자열" << endl;
+
+    string str1;     // 빈 문자열
+    string str2 = "Hello, C++!"; // 문자열 초기화
+    string str3(str2);  // 문자열 복사
+    string str4(str2, 0, 5);  // Hello 복사
+    string str5(10, '*');   // **********
+
+    // 문자열 찾기
+    cout << "문자열 찾기" << endl;
+
+    string str = "Hello, C++ World!";
+
+    // Hello 문자열 찾기
+    size_t pos1 = str.find("Hello");
+    cout << pos1 << endl;   // 0
+
+    // 'C' char 찾기
+    size_t pos2 = str.find('C');
+    cout << pos2 << endl;   // 7
+
+    // 시작 인덱스 지정하고 Hello 조회
+    size_t start_index = 2;  // l 부터 시작해서 찾기
+    size_t pos3 = str.find("Hello", start_index);
+    cout << pos3 << endl;    // 18446744073709551615   -> 인덱스를 찾을 수 없음
+
+    // 존재하지 않는 문자열 조회
+    size_t pos4 = str.find("Python");
+    cout << pos4 << endl;  // 18446744073709551615  -> Python은 문자열에 없음
+
+    // 문자열 추가, 수정
+    str = "APPLE";
+    str += ", World!";   // 문자열 추가
+    cout << str << endl;     // APPLE, World!
+
+    str[7] = 'P'; // W -> P
+    cout << str << endl;     // APPLE, Porld!
+
+    str.replace(7, 4, "Col");  // 7번째 인덱스 글자부터 4글자를 Col로 변경
+    //str.replace(7, 3, "Co");  // 7번째 인덱스 글자부터 3글자를 Co로 변경
+    cout << str << endl;     // APPLE, Cold!
+
+    return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
