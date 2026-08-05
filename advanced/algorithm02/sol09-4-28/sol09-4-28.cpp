21b2008ee26fc901cd9c05972eefae1116595001
Hugo MG Sung <personar95@naver.com>
2026-08-05 14:10:11 +0900

    알고리즘 고급, 트리까지

--- advanced/algorithm02/sol09-4-28/sol09-4-28.cpp
+++ advanced/algorithm02/sol09-4-28/sol09-4-28.cpp
@@ -20,17 +20,49 @@ string preorder(vector<int> nodes, int idx) {
     }
 }
 
+// 중위 순회
+string inorder(vector<int> nodes, int idx) {
+    if (idx < nodes.size()) {  // nodes가 크기 7인데 인덱스가 7보다 커질 수 없다
+        string ret = inorder(nodes, idx * 2 + 1);  // 인덱스가 0부터 시작
+        ret += to_string(nodes[idx]) + " ";
+        ret += inorder(nodes, idx * 2 + 2);
+
+        return ret;
+    }
+    else {
+        return "";
+    }
+}
+
+// 후위 순회
+string postorder(vector<int> nodes, int idx) {
+    if (idx < nodes.size()) {  // nodes가 크기 7인데 인덱스가 7보다 커질 수 없다
+        string ret = postorder(nodes, idx * 2 + 1);  // 인덱스가 0부터 시작
+        ret += postorder(nodes, idx * 2 + 2);
+        ret += to_string(nodes[idx]) + " ";
+
+        return ret;
+    }
+    else {
+        return "";
+    }
+}
+
+
+
 vector<string> solution(vector<int> nodes) {
     vector<string> answer;
 
     // 전위 순회 : 부모->왼->오른
     string pre_result = preorder(nodes, 0);
-
     // 중위 순회 : 왼->부모->오른
-
+    string in_result = inorder(nodes, 0);
     // 후위 순회 : 왼->오른->부모
+    string post_result = postorder(nodes, 0);
 
     answer.push_back(pre_result);
+    answer.push_back(in_result);
+    answer.push_back(post_result);
 
     return answer;
 }
